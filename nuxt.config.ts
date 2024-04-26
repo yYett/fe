de6ee666d3d5dd546c0aa9bda543041292c119ba
yYett <56874009+yYett@users.noreply.github.com>
2024-04-27 00:03:03 +0100

    feature: style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  extends: ["./layer-login"],
+  components: {
+    global: true,
+    dirs: ["~/components"],
+  },
+  css: ["~/assets/scss/base/index.scss"],
+});
