5cd914ad99a1365b5b9702a4f775211c4ff80185
yYett <56874009+yYett@users.noreply.github.com>
2024-05-12 12:17:26 +0100

    feature: DescriptiveLink component

---
+++ layer-login/components/DescriptiveLink.vue
@@ -0,0 +1,30 @@
+<template>
+  <p class="descriptive-link f-caption">
+    {{ text }}
+    <button class="btn btn--link" type="button" @click="emit('clicked', true)">
+      {{ label }}
+    </button>
+  </p>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  text: string;
+  label: string;
+}>();
+
+const emit = defineEmits<{
+  clicked: [value: boolean];
+}>();
+</script>
+
+<style scoped lang="scss">
+.descriptive-link {
+  --font-w: 400;
+
+  display: flex;
+  align-items: baseline;
+  justify-content: center;
+  gap: 0.5rem;
+}
+</style>
